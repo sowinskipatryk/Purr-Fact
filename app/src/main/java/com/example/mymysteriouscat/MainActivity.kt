a89288d7f12478325b4fbf5742298b14e0132195
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-07-05 00:53:47 +0200

    working app

--- app/src/main/java/com/example/mymysteriouscat/MainActivity.kt
+++ app/src/main/java/com/example/mymysteriouscat/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.mymysteriouscat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
-import android.widget.Button
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import okhttp3.*
+import org.json.JSONObject
 import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,10 +22,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val funFactButton: Button? = findViewById(R.id.funFactButton)
-        funFactButton?.setOnClickListener{
+        supportActionBar?.hide()
+
+        val funFactButton: ImageButton = findViewById(R.id.funFactButton)
+        funFactButton.setOnClickListener{
             run("https://catfact.ninja/fact")
         }
+        funFactButton.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(arg0: View?, arg1: MotionEvent): Boolean {
+                if (arg1.action == MotionEvent.ACTION_DOWN)
+                {
+                    funFactButton.setImageResource(R.drawable.button_paw_clicked)
+                } else {
+                    funFactButton.setImageResource(R.drawable.button_paw)
+                    run("https://catfact.ninja/fact")
+                }
+                return true
+            }
+        })
     }
 
     fun run(url: String) {
@@ -30,7 +51,14 @@ class MainActivity : AppCompatActivity() {
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {}
-            override fun onResponse(call: Call, response: Response) = funFactTextView.setText(response.body()?.string())
+            override fun onResponse(call: Call, response: Response){
+                Handler(Looper.getMainLooper()).post(Runnable {
+                    val jsonString = response.body()?.string()
+                    val jsonObject = JSONObject(jsonString)
+                    val data = jsonObject.getString("fact")
+                    funFactTextView.setText(data)
+                })
+            }
         })
     }
 }
